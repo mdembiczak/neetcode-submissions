5c36cb26d91c3792e925767fc6f90fba2de2d966
Maciej Dembiczak <mdembiczak96@gmail.com>
2026-09-21 13:57:51 +0200

    Add: validate-parentheses - submission-9

---
+++ Data Structures & Algorithms/validate-parentheses/submission-9.py
@@ -0,0 +1,14 @@
+class Solution:
+    def isValid(self, s: str) -> bool:
+        
+        stack = deque()
+        for c in s:
+            if c == "(":
+                stack.append(")")
+            elif c == "{":
+                stack.append("}")
+            elif c == "[":
+                stack.append("]")
+            elif len(stack) == 0 or c != stack.pop():
+                return False
+        return len(stack) == 0
